870a2bb5012d281e18bdeb3a00d701073d73f2b3
Rai <rai@ingmechs.com>
2022-08-08 23:27:15 +0200

    Init recon scanner

---
+++ src/python_nmap/recon_host_scanner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+recon_host_scanner.py - Scan a host to find open ports.
+
+Usage: python recon_host_scanner.py <ip_address>
+
+Author: @raiabril
+"""
+
+import nmap
+
+
